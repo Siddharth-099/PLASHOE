4afe53a93a3cf8c858be705d88d5ba34cc1972a9
Siddharth-099 <githubsiddhu@gmail.com>
2026-09-11 15:00:57 +0530

    adding code to allow host

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    allowedHosts: ['plashoe-2nyc.onrender.com'],
+  },
 })
